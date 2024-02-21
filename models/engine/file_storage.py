feeb9914ca50d000882a21ab61675635677df2ff
eyramakua <eyramagginie@gmail.com>
2024-02-21 08:26:11 -0800

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,8 @@ class FileStorage:
             for id, dict in temp.items():
                 temp_instance = models.dummy_classes[dict["__class__"]](**dict)
                 self.__objects[id] = temp_instance
-        pass
+        except:
+            pass
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects """
