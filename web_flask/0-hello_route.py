8fffff97f5c977f41d35475b25ba0170bbff4387
eyramakua <eyramagginie@gmail.com>
2024-03-24 14:22:09 -0700

    correction of task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slahes=False)
+@app.route('/', strict_slashes=False)
 def index():
     """function to return Hellow HBNB"""
     return 'Hello HBNB!'
